41321d7e880b4f404cd5d385e65bd1e06fab084a
fajaralf <202310072@student.ibik.ac.id>
2022-04-07 21:54:20 +0700

    Web Wedding Organizer

---
+++ frontend/views/site/about.php
@@ -0,0 +1,76 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = "About Us";
+$this->params["breadcrumbs"][] = $this->title;
+?>
+<div class="site-about">
+    <div class="jumbotron text-center bg-transparent">
+        <h1>Meet Our Crew</h1>
+        <p>
+            WE SET OUR MINDS TO SUPPORT AND GUIDE OUR CLIENTS AS WELL AS TO CURATE EACH EVENT. 
+            WE ARE A CREATIVE, BRIGHT TEAM OF MULTI-LINGUAL PLANNERS WHO PLACE INTEGRITY, 
+            CREATIVITY AND ATTENTION TO DETAIL AT THE CORE OF ALL WE DO.
+        </p>
+    
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="jumbotron text-center bg-transparent">
+                    <p>
+                        <img src="../../web/assets/images/crew/lukman.jpg" alt="Lukman" class="img-thumbnail"> 
+                    </p>
+                    <p>Lukman</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="jumbotron text-center bg-transparent">
+                    <p>
+                        <img src="../../web/assets/images/crew/putri.jpg" alt="Putri" class="img-thumbnail">
+                    </p>
+                    <p>Putri</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="jumbotron text-center bg-transparent">
+                    <p>
+                        <img src="../../web/assets/images/crew/rahma.jpg" alt="Rahma" class="img-thumbnail">
+                    </p>
+                    <p>Rahma</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="jumbotron text-center bg-transparent">
+                    <p>
+                        <img src="../../web/assets/images/crew/yasin.jpg" alt="Yasin" class="img-thumbnail">
+                    </p>
+                    <p>Yasin</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="jumbotron text-center bg-transparent">
+                    <p>
+                        <img src="../../web/assets/images/crew/fajar.jpg" alt="Fajar" class="img-thumbnail">
+                    </p>
+                    <p>Fajar</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="jumbotron text-center bg-transparent">
+                    <p>
+                        <img src="../../assets/images/crew/eko.jpg" alt="Eko" class="img-thumbnail">
+                    </p>
+                    <p>Eko</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
